a2e81778b09442d0002c5ec009f17aaa8fdf3848
archana404 <64302204+archana404@users.noreply.github.com>
2020-04-25 13:11:10 +0530

    Add files via upload

---
+++ Encryption_Decryption.py
@@ -0,0 +1,62 @@
+def find_in_list(query, mainlist):
+# this function is used to find the position of the "query" in the "mainlist". If "query" is in the list then it returns its position, otherwise it returns None
+    mainlist_len = len(mainlist)
+    index = None
+    for i in range(mainlist_len):
+        element = mainlist[i]
+        if element == query:
+            index = i
+            return i
+# this should return the position of the "query" in the "mainlist"
+
+
+chars =         ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+shifted_chars = ['c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b']
+
+# encrypt_message function defined here but not called
+def encrypt_message(plain_msg):
+# this fucnction takes "plain_msg" as an argument and print/return the encrypted message. The "plain_msg" is tranfered into "encrypted_msg" using "shifted_chars" list. Example, if plain_msg = "ng" then n => p, g => i  and hence encrypted_msg = "pi"
+    encrypted_msg = ""
+    for character in plain_msg:
+      # for character in msg
+        if character in chars:
+            char_index = find_in_list(character, chars)
+            new_char = shifted_chars[char_index]
+            encrypted_msg = encrypted_msg + new_char
+        else:
+            encrypted_msg = encrypted_msg + character
+    print(encrypted_msg)
+# decrypt_message function defined here but not called
+def decrypt_message(encrypted_msg):
+# this fucnction takes "encrypted_msg" as an argument and print/return the encrypted message. The "encrypted_msg" is tranfered into "decrypted_msg" using "shifted_chars" list. Example, if encrypted_msg = "pi" then p => n, i => g  and hence decrypted_msg = "ng"
+    decrypted_msg = ""
+    for character in encrypted_msg:
+        if character in shifted_chars:
+            char_index = find_in_list(character, shifted_chars)
+            new_char = chars[char_index]
+            decrypted_msg = decrypted_msg + new_char
+        else:
+            decrypted_msg = decrypted_msg + character
+    print(decrypted_msg)
+
+# methods should return or print the new messages.
+############################################### Code starts from here ##################################################
+flag = True
+while flag == True:
+  choice = input("What do you want to do? 1. Encrypt a message 2. Decrypt a message  Enter `e` or `d` respectively!")
+  if choice == 'e':
+      plain_message = input("Enter message to encrypt?")
+      encrypt_message(plain_message)
+  elif choice == 'd':
+      encrypted_msg = input("Enter message to decrypt?")
+      decrypt_message(encrypted_msg)
+  else:
+      play_again = input("Do you want to try agian or Do you want to exit? (Y/N)").lower()
+      if play_again == 'yes':
+          continue
+      elif play_again == 'no':
+          break
+      elif play_again == "":
+          break
+      else:
+          break
